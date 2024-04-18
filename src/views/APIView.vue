0f42baa2cbde4d4324453cbf91f3a85874b271f9
Eduardo Teles <elbteles@hotmail.com>
2024-04-18 20:41:01 +0100

    updated post and put

--- src/views/APIView.vue
+++ src/views/APIView.vue
@@ -1,27 +1,57 @@
 <template>
     <div>
-      <button @click="getUsers">Get users</button><br/>
-  
-      <div class="users" v-for="user in users" :key="user.id">
-        <h2>{{ user.name }}</h2>
-        <p>{{ user.email }}</p>
-      </div>
+        <!-- HTTP GET -->
+        <button @click="getUsers">Get users</button>
+        <div v-for="user in users" :key="user.id">
+            <h2>{{ user.id }}</h2>
+            <p>{{ user.name }}</p>
+            <p>{{ user.username }}</p>
+            <p>{{ user.email }}</p>
+        </div>
+    </div>
+        <!-- HTTP POST -->
+        <h2>Add user</h2>
+        <input type="text" v-model="name" /></br>
+        <input type="text" v-model="email" /></br>
+        <button @click="postUser">Post User</button>
+    <div>
+        
     </div>
 </template>
-  
+
 <script setup>
-  import axios from "axios";
-  import { ref } from 'vue';
+    import {ref} from 'vue'
+    import axios from 'axios'
+    const users = ref([])
+    const name = ref("")
+    const email = ref("")
 
-  const users = ref([]);
-
-  const getUsers = async () => {
-    try {
-      const response = await axios.get("https://jsonplaceholder.typicode.com/users");
-      users.value = response.data;
-    } catch (e) {
-      console.error(e);
+    // HTTP GET
+    const getUsers = async () => {
+        try{
+            const response = await axios.get("https://jsonplaceholder.typicode.com/users")
+            users.value = response.data
+        }
+        catch(e){
+            console.error(e)
+        }
     }
-  };
+
+    // HTTP POST
+    const postUser = async () => {
+        try{
+            const user = await axios.post(
+                "https://jsonplaceholder.typicode.com/users",
+                {
+                    name: name.value,
+                    email: email.value
+                }
+            )
+            console.log(user)
+            getUsers()
+        }
+        catch(e){
+            console.error(e)
+        }
+    }
 </script>
-    
---
+++ src/views/testing.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <button @click="getUsers">Get users</button><br/>
+  
+      <div class="users" v-for="user in users" :key="user.id">
+        <h2>{{ user.name }}</h2>
+        <p>{{ user.email }}</p>
+      </div>
+    </div>
+</template>
+  
+<script setup>
+  import axios from "axios";
+  import { ref } from 'vue';
+
+  const users = ref([]);
+
+  const getUsers = async () => {
+    try {
+      const response = await axios.get("https://jsonplaceholder.typicode.com/users");
+      users.value = response.data;
+    } catch (e) {
+      console.error(e);
+    }
+  };
+</script>
+    
